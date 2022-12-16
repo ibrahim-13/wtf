3d31c5b023e98a550292b0073d4d8417eba3e2b2
ibrahim-13 <ib.arshad777@gmail.com>
2022-12-16 23:06:32 +0600

    Sync kraken api calls

--- modules/kraken/common/request.go
+++ modules/kraken/common/request.go
@@ -6,7 +6,16 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"sync"
+	"time"
 )
+
+const (
+	__req_duration = 1 * time.Second
+)
+
+var __req_time time.Time = time.Now().Add(-1 * time.Second)
+var __req_time_lock sync.Mutex
 
 func GetKrakenStatus() (*KrakenResponse[KrakenDataStatus], error) {
 	return executeRequest[KrakenDataStatus](EndpointSystemStatus)
@@ -19,6 +28,11 @@ func GetKrakenTicker(pairs ...string) (*KrakenResponse[KrakenDataTicker], error)
 }
 
 func executeRequest[K interface{}](url string) (*KrakenResponse[K], error) {
+	__req_time_lock.Lock()
+	now := time.Now()
+	if now.Add(__req_duration).After(__req_time) {
+		<-time.After(__req_duration)
+	}
 	response, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -28,6 +42,8 @@ func executeRequest[K interface{}](url string) (*KrakenResponse[K], error) {
 		return nil, err
 	}
 	data := KrakenResponse[K]{}
+	__req_time = time.Now()
+	__req_time_lock.Unlock()
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		return nil, err
